75522fa5357d0017aea276d9828a3368f0c641fd
Doyeon Kim <90243955+Doyeon-k-fanshawe@users.noreply.github.com>
2023-04-04 23:48:40 -0400

    ADD: ALL

--- public/js/components/TheHomePage.js
+++ public/js/components/TheHomePage.js
@@ -2,14 +2,106 @@ export default {
   name: 'TheHomePageComponent',
 
   template: `
-    <h1>This is the default home page.</h1>
+  <!--Navbar-->
+  <nav id="navbar">
+  <div class="navbar__box">
+    <h2>_ROKU FLASHBACK_</h2>
+    <ul class="navbar__menu">
+      <li class="navbar__menu__item"><a href="/"><h4>MOVIES</h4></a></li>
+      <li class="navbar__menu__item"><a href="/"><h4>TV_SERIES</h4></a></li>
+      <li class="navbar__menu__item"><a href="/"><h4>MUSIC</h4></a></li>
+      <li class="navbar__menu__item"><a href="/"><h4>WATCH_LIST</h4></a></li>
+    </ul>
+    <div class="navbar__serachbar">
+      <input>
+      <i class="fa-solid fa-magnifying-glass"></i>
+    </div>
+    <div class="navbar__user">
+      <img src="images/amelia.png" alt="User Image">
+    </div>
+  </div>
+  </nav>
+
+  <!-- HERO IMG-->
+  <div id="hero__img">
+  </div>
+
+  <!--Main-->
+  <section id="home">
+    <!--Top 5-->
+    <div class="topFive">
+      <h3>TOP5_IN_CANADA</h3>
+      <div class="topFive__movie">
+      </div>
+    </div>
+
+    <!--TV Show-->
+    <div class="tvShows">
+      <h3>TV_SHOWS</h3>
+      <div class="tvshows__list"></div>
+    </div>
+
+    <!--Music-->
+    <div class="music">
+      <h3>MUSIC</h3>
+      <div class="music__box">
+        <div class="music__box__item">
+          <img src="images/mia.png" alt="Music Image">
+        </div>
+        <div class="music__box__item">
+          <img src="images/mia.png" alt="Music Image">
+        </div>
+        <div class="music__box__item">
+          <img src="images/mia.png" alt="Music Image">
+        </div>
+        <div class="music__box__item">
+          <img src="images/mia.png" alt="Music Image">
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <!--Footer-->
+  <footer id="footer">
+    <div class="backToTop">
+      <a href="/">
+        <h3>BACK_TO_TOP</h3>
+        <i class="fa-solid fa-caret-up"></i>
+      </a>
+    </div>
+    <h2>_ROKU FLASHBACK_</h2>
+    <h4>
+      UNLINITED_MOVIES_TV_SHOWS_AND_MORE
+      MORE_STORIES_THAN_EVER_BEFORE
+    </h4>
+    <p>Copyright © 2023 _ROKU FLASHBACK_</p>
+  </footer>
   `,
 
   created() {
-    // fetch('hit the IMDB API using the reference video I gave you last semester')
-    // this will load video content, your video thumbnails, ect.
+    // Movies 
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '27f4f88fb9msh56e4535fa65bdfap1f539bjsn7c01bb61481d',
+        'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
+      }
+    };
 
-    // go to rapidAPI, get a key, find the IMDB API and start exploring.
-    
+    fetch('https://online-movie-database.p.rapidapi.com/auto-complete?q=game%20', options)
+      .then(response => response.json())
+      .then(data => {
+        const list = data.d;
+      
+        list.map((item) => {
+          const name = item.l;
+          const poster = item.i.imageUrl;
+          const movie = `<li><img src="${poster}"><h2>${name}</h2></li>`;
+          document.querySelector(".topFive__movie").innerHTML += movie;
+          document.querySelector(".tvshows__list").innerHTML += movie;
+        })
+      })
+      .catch(err => console.error(err));
   }
 }
--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const router = express.Router();
 // by default, you can't access URLs that are different than your own (different origin points) - this is a security features built into the Web. However, you CAN use an intermediary to "break into" or get access to other domains and do things like data retrieval, ect - think of this as a virtual "swipe card" that allows access. http-proxy-middleware is the swipe card - it's configured to allow access to an endpoint and let you use that domain. The target in this case is our back end Roku service (the database with all of the users) - we can retrieve them and show them in our UI with the middleware's access configured correctly.
 
 router.use("/", createProxyMiddleware({
-  target: 'http://localhost:5050',
+  target: 'http://localhost:4000',
   headers: {
     accept: "application/json, application/x-www-form-urlencoded"
   },
